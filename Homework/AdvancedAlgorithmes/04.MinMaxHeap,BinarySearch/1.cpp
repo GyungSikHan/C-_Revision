d272ad5b54451af26baad76cc903e711d2695b2e
GyungSikHan <gksrudtlr2@icloud.com>
2026-08-26 08:46:06 +0900

    Add 임시 저장

---
+++ Homework/AdvancedAlgorithmes/04.MinMaxHeap,BinarySearch/1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int length = 10;
+int arr[length]{ 4,4,5,7,8,10,20,22,23,25 };
+
+bool BinarySearch(const int target, int start, int end)
+{
+	int mid = (start + end) / 2;
+	int temp = arr[mid];
+	if (temp == target)
+		return true;
+	else if (temp < target)
+		return BinarySearch(target, mid+1, end);
+	else
+		return BinarySearch(target, start, mid-1);
+
+	return false;
+}
+int main()
+{
+	int target{};
+	cin >> target;
+
+	cout << (BinarySearch(target, 0, length - 1) == true ? "O" : "X") << endl;
+}
---
+++ Homework/AdvancedAlgorithmes/04.MinMaxHeap,BinarySearch/2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int MAX = 100000;
+
+int n{};
+string strArr[MAX]{};
+int m{};
+
+int main()
+{
+	
+}
